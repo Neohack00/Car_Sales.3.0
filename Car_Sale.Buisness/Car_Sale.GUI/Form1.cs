b5b1ff16c57269e64e3559a67a5b19fd6b917b56
Neohack00 <neo@bylock.se>
2019-10-07 20:05:02 +0200

    Car_Sale.GUI: Input data(textBox1/Car.Brand), Store data(Arraylist) , Repost(Show cars).

--- Car_Sale.Buisness/Car_Sale.GUI/Form1.cs
+++ Car_Sale.Buisness/Car_Sale.GUI/Form1.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Car_Sale.Business;
+using System.Collections;
 
 namespace Car_Sale.GUI
 {
+    
     public partial class Form1 : Form
     {
+        List<Car> MyCars = new List<Car>();
+
         public Form1()
         {
             InitializeComponent();
+            button1.Text = "Add Car";
+            button2.Text = "Show Cars";
+            label1.Text = "Brand";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyCars.Add(new Car() { Brand = textBox1.Text });
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (Car x in MyCars)
+            {               
+                listBox1.Items.Add(x.Brand);
+            }
         }
     }
 }
